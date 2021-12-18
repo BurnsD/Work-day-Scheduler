37a1184b8a09ef86c706cb5d18868d5aeb6817ba
BurnsD <Dustinburnsactech@gmail.com>
2021-12-18 00:49:58 -0600

    Local storage in js, updated textarea class

--- Develop/script.js
+++ Develop/script.js
@@ -23,3 +23,23 @@ function background () {
  }
 
  background ();
+// -- Local Storage --
+ $(document).ready(function () {
+    // Button saves upon being clicked
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        // Saves text and time to local storage
+        localStorage.setItem(text, time);
+    })  
+}); 
+// Gets description data from local storage
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
